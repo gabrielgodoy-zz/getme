c137f52f403c8f8fa2203309549b5e7d82155659
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-05-01 20:21:13 -0300

    whoops

--- stubs/weather.js
+++ stubs/weather.js
@@ -69,9 +69,9 @@ const weatherResponseMock = JSON.stringify({
   dt: 1483567200,
 });
 
-export {weatherResponseMock};
+exports.weatherResponseMock = weatherResponseMock;
 
-export function getIconMocks() {
+exports.getIconMocks = function getIconMocks () {
   ['02d','03d','04d','09d','10d','11d','13d','50d','01n','02n','03n','04n','09n','10n','11n','13n','50n']
     .map(icon => {
       const res = Object.assign({}, weatherResponseMock)
