aa7f4ad5165c8bea109fae46ffbdbc8fbcd1af46
Wallace Coelho <wac.almeida@gmail.com>
2017-06-04 20:39:38 -0300

    Automated Tests

--- src/options/optDate.js
+++ src/options/optDate.js
@@ -1,22 +1,25 @@
 const chalk = require('chalk');
 const moment = require('moment/min/moment-with-locales');
 
-function optDate(value, data) {
-  const date = moment(data.date);
-  const lang = 'pt';
-  const originalDate = date.format('YYYY-MM-DD');
-  moment.locale(lang);
-  const period = data.period || 'd';
-
-  let newDate;
-  if (data.subtract) {
-    newDate = date.subtract(value, period);
+function calculateDate(date, period, value, subtract) {
+  const dt = moment(date);
+  if (subtract) {
+    dt.subtract(value, period);
   } else {
-    newDate = date.add(value, period);
+    dt.add(value, period);
   }
-  const formatedDate = newDate.format('(ddd) YYYY-MM-DD');
-  console.log(`\nInformed date: ${originalDate}`);
-  return console.log(`Calculated Date: ${chalk.yellow(formatedDate)}`);
+  return dt;
+}
+
+function optDate(value, data) {
+  const formatString = '(ddd) YYYY-MM-DD';
+  const period = data.period || 'd';
+  const date = moment(data.date);
+  const newDate = calculateDate(date, period, value, data.subtract);
+  const sDate = date.format(formatString);
+  const sNewDate = newDate.format(formatString);
+  console.log(`\nInformed date: ${chalk.yellow(sDate)}`);
+  return console.log(`Calculated Date: ${chalk.yellow(sNewDate)}`);
 }
 
 module.exports = optDate;
