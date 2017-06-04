b225b589dbec324f6997d1354b42ef268f16cc24
nipher <jonas@nipher.io>
2017-06-04 00:11:30 -0300

    Update optTwitter to esnext + axios + specs

--- src/options/optTwitter.test.js
+++ src/options/optTwitter.test.js
@@ -1,26 +1,53 @@
 /* eslint-disable no-unused-expressions */
+/* eslint no-underscore-dangle: ["error", { "allow": ["__set__"] }] */
 
-const chai = require('chai');
-const sinonChai = require('sinon-chai');
-const sinon = require('sinon');
-const optTwitter = require('./optTwitter');
+import moxios from 'moxios';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import sinon from 'sinon';
+import chalk from 'chalk';
+import rewire from 'rewire';
 
+const optTwitter = rewire('./optTwitter');
 
 chai.use(sinonChai);
-let queryMock;
+
+const responseMock = {
+  statuses: [
+    {
+      user: { name: 'test_user' },
+      text: 'test_tweet',
+      created_at: 'test',
+    },
+  ],
+};
+
+let consoleStub;
+let fakeGetAccessToken;
 
 describe('optTwitter', () => {
   beforeEach(() => {
-    queryMock = ['arg1', 'arg2'];
-    sinon.stub(console, 'log');
+    fakeGetAccessToken = sinon.stub().returns('fake_token');
+    optTwitter.__set__({ getAccessToken: fakeGetAccessToken });
+    moxios.install();
+    consoleStub = sinon.stub(console, 'log');
   });
 
   afterEach(() => {
+    moxios.uninstall();
     console.log.restore();
   });
 
-  it('Should call \' searchTwitter()\' and console log tweets', (done) => {
-    optTwitter(queryMock);
-    done();
+  it('should console log tweets', async () => {
+    optTwitter(['test', 'test2']);
+
+    await new Promise(resolve => moxios.wait(resolve));
+    const request = moxios.requests.mostRecent();
+    await request.respondWith({ status: 200, response: responseMock });
+
+    expect(fakeGetAccessToken).to.have.been.called;
+    expect(consoleStub).to.have.been.calledWith(`${chalk.yellow('From:')} test_user`);
+    expect(consoleStub).to.have.been.calledWith(`${chalk.yellow('Tweet:')} test_tweet`);
+    expect(consoleStub).to.have.been.calledWith(`${chalk.yellow('Time:')} test\n`);
   });
 });
