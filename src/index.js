5327d27772779b2b6a2fb9ff12c36428b3f5143d
Gabriel Godoy <hi@gabrielgodoy.com>
2017-06-09 13:34:51 -0300

    Fix/readme refactor (#58)
    * small fixes
    
    * 1.4.3
    
    * Version bump
    
    * 1.4.7
    
    * Readme Refactor
    
    * 1.4.8

--- src/index.js
+++ src/index.js
@@ -17,23 +17,9 @@ const optTwitter = require('./options/optTwitter');
 const optDate = require('./options/optDate');
 
 commander
-  .version(currVersion)
-  .command('weather')
-  .alias('w')
-  .description('Get weather')
-  .option('-c, --celsius', 'Get the weather in celsius unit (Default)')
-  .option('-f, --fahrenheit', 'Get the weather in fahrenheit unit')
-  .option('-k, --kelvin', 'Get the weather in kelvin unit')
-  .action(command => optWeather(command));
-
-commander
-  .command('forecast')
-  .alias('f')
-  .description('Get weather forecast of five days ahead')
-  .option('-c, --celsius', 'Get the weather in celsius unit (Default)')
-  .option('-f, --fahrenheit', 'Get the weather in fahrenheit unit')
-  .option('-k, --kelvin', 'Get the weather in kelvin unit')
-  .action(command => optWeather(command));
+  .command('chuck')
+  .description('Get Chuck Norris facts')
+  .action(command => optChuckNorris(command));
 
 commander
   .command('currency')
@@ -44,27 +30,21 @@ commander
   .action(command => optCurrency(command));
 
 commander
+  .command('date <value>')
+  .description('Add or subtract date')
+  .alias('dt')
+  .option('-d, --date <startDate>', 'Sets the start date (Default: Current Date)')
+  .option('-p, --period <period>', 'Sets the period. Day(d), Month(M), Year(y), Week(w) (Default: d)', 'd')
+  .option('-a, --add', 'Inform to add value to date (Default Option)', true)
+  .option('-s, --subtract', 'Inform to subtract value from date', false)
+  .action((value, command) => optDate(value, command));
+
+commander
   .command('definition')
   .alias('d')
   .description('Get definition of words')
   .option('-s, --synonym <word>', 'Get synonyms of a specific word')
   .action(command => optDefinition(command));
-
-commander
-  .command('translation [text...]')
-  .alias('t')
-  .description('Get translations for words')
-  .option('-l, --list', 'List all possible language combinations of translation to insert on --fromto')
-  .option('-f, --fromto <languages>', 'The translation direction. As a pair of language codes separated by a hyphen ("from"-"to"). For example, en-ru indicates translating from English to Russian.')
-  .option('-t, --text [text...]', 'Insert all the text you want to translate after this flag')
-  .action((textToTranslateArray, options) => optTranslate(textToTranslateArray, options));
-
-commander
-  .command('upload <filepath>')
-  .alias('u')
-  .description('Uploads a file and generates a link for download')
-  .option('-e, --expiration <expiration>', 'Set custom time for generated link to expirate, default is 14 days')
-  .action((file, options) => optUpload(file, options));
 
 commander
   .command('ip')
@@ -78,9 +58,18 @@ commander
   .action(command => optQuote(command));
 
 commander
-  .command('speed')
-  .description('Get the speed of your connection')
-  .action(command => optSpeed(command));
+  .command('forecast')
+  .alias('f')
+  .description('Get weather forecast of five days ahead')
+  .option('-c, --celsius', 'Get the weather in celsius unit (Default)')
+  .option('-f, --fahrenheit', 'Get the weather in fahrenheit unit')
+  .option('-k, --kelvin', 'Get the weather in kelvin unit')
+  .action(command => optWeather(command));
+
+commander
+  .command('git')
+  .description('Notifications from GitHub')
+  .action(command => optGit(command));
 
 commander
   .command('search [query...]')
@@ -89,31 +78,41 @@ commander
   .action(query => optSearch(query));
 
 commander
+  .command('speed')
+  .description('Get the speed of your connection')
+  .action(command => optSpeed(command));
+
+commander
+  .command('translation [text...]')
+  .alias('t')
+  .description('Get translations for words')
+  .option('-l, --list', 'List all possible language combinations of translation to insert on --fromto')
+  .option('-f, --fromto <languages>', 'The translation direction. As a pair of language codes separated by a hyphen ("from"-"to"). For example, en-ru indicates translating from English to Russian.')
+  .option('-t, --text [text...]', 'Insert all the text you want to translate after this flag')
+  .action((textToTranslateArray, options) => optTranslate(textToTranslateArray, options));
+
+commander
   .command('tweets [query...]')
   .alias('tw')
   .description('Search string on Twitter')
   .action(query => optTwitter(query));
 
 commander
-  .command('chuck')
-  .description('Get Chuck Norris facts')
-  .action(command => optChuckNorris(command));
+  .command('upload <filepath>')
+  .alias('u')
+  .description('Uploads a file and generates a link for download')
+  .option('-e, --expiration <expiration>', 'Set custom time for generated link to expirate, default is 14 days')
+  .action((file, options) => optUpload(file, options));
 
 commander
-  .command('date <value>')
-  .description('Add or subtract date')
-  .alias('dt')
-  .option('-d, --date <startDate>', 'Sets the start date (Default: Current Date)')
-  .option('-p, --period <period>', 'Sets the period. Day(d), Month(M), Year(y), Week(w) (Default: d)', 'd')
-  // .option('-v, --value <value>', 'Sets the value for period')
-  .option('-a, --add', 'Inform to add value to date (Default Option)', true)
-  .option('-s, --subtract', 'Inform to subtract value from date', false)
-  .action((value, command) => optDate(value, command));
-
-commander
-  .command('git')
-  .description('Notifications from GitHub')
-  .action(command => optGit(command));
+  .version(currVersion)
+  .command('weather')
+  .alias('w')
+  .description('Get weather')
+  .option('-c, --celsius', 'Get the weather in celsius unit (Default)')
+  .option('-f, --fahrenheit', 'Get the weather in fahrenheit unit')
+  .option('-k, --kelvin', 'Get the weather in kelvin unit')
+  .action(command => optWeather(command));
 
 commander.on('*', (command) => {
   console.log(`The command "${chalk.red(command)}" does not exist`);
